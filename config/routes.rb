a16b2e398ed2551d06abd24624c4b551a7cd554b
MaryAninzo <aninzom211@gmail.com>
2021-11-24 09:59:35 -0500

    React setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # config/routes.rb
-  # get "/hello", to: "application#hello_world"
+  get "/hello", to: "application#hello_world"
 end
 
 
