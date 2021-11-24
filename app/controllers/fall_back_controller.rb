47a035bf579812336ed1e400c1ba3bc1246f28e9
MaryAninzo <aninzom211@gmail.com>
2021-11-24 10:18:13 -0500

    Added configuration files

---
+++ app/controllers/fall_back_controller.rb
@@ -0,0 +1,7 @@
+# app/controllers/fallback_controller.rb
+class FallbackController < ActionController::Base
+    def index
+      render file: 'public/index.html'
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # config/routes.rb
   get "/hello", to: "application#hello_world"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+end
+
 end
 
 
