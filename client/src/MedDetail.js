31dc2a47c52035b9af9a86e674b8a79d41ad16f6
MaryAninzo <aninzom211@gmail.com>
2021-11-30 22:15:35 -0500

    stuff

--- client/src/MedDetail.js
+++ client/src/MedDetail.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom'
 
 function MedDetail( ) {
+    // const {id} =useParams();
     const [med, setMed] = useState("null");
     const [isLoaded, setIsLoaded] =useState(false);
     const id = useParams().id;
@@ -12,12 +13,13 @@ function MedDetail( ) {
             setMed(med)
             setIsLoaded(true);
         });
-    },);
+    },[]);
 
     if (!isLoaded) return <h2>Loading...</h2>;
     
     return (
         <div className="med-Detail">
+        
             <img src={med.image} alt={med.name}/>
             <h4>{med.name}</h4>
             <p>{med.description}</p>
@@ -28,3 +30,11 @@ function MedDetail( ) {
 }
 
 export default MedDetail;
+
+
+// {medications.filter((med) => med.id === id).map((med) => (
+//     <div className="full-card" key={med.id}>
+//         <h2>Name: {med.name}</h2>
+//         <h4>{med.description}</h4>
+//         </div>
+// ))}
